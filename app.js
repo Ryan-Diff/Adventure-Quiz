8495bf34fb6d6d4e137010d52f06efc694946fe5
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-23 11:55:13 -0700

    Added a form w/ local storage

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // import functions and grab DOM elements
-
+import { makeUser } from './userUtils.js';
+const form = document.querySelector('form');
 // initialize state
 
 // set event listeners to update state and DOM
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const user = makeUser(formData);
+
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem('USER', stringyUser);
+
+
+});
